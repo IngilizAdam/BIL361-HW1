deb4ea19e87f0056a41e1e4bc139754a256411bf
IngilizAdam <hasantuna0070@gmail.com>
2022-10-30 00:29:05 +0300

    testbench update

--- BIL361_HW1.runs/synth_1/islemci.tcl
+++ BIL361_HW1.runs/synth_1/islemci.tcl
@@ -95,6 +95,8 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
 set_param ips.enableIPCacheLiteLoad 1
+
+read_checkpoint -auto_incremental -incremental C:/Users/hasan/Desktop/Projects/Vivado/BIL361_HW1/BIL361_HW1.srcs/utils_1/imports/synth_1/islemci.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
